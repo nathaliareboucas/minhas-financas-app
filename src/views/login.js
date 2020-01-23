e23a125acf5f7b2001c55023bd94276a1b2e79e3
Nathalia <nathreboucas5@gmail.com>
2020-01-23 16:25:59 -0300

    implementando o componente de login

--- src/views/login.js
+++ src/views/login.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 import Card from '../components/card'
+import FormGroup from '../components/form-group'
 
 class Login extends React.Component {
+
+	state = {
+		email: '',
+		senha: ''
+	}
+
+	entrar = () => {
+		console.log('Email: ', this.state.email)
+		console.log('Senha: ', this.state.senha)
+	}
 
 	render() {
 		return (
@@ -10,9 +21,36 @@ class Login extends React.Component {
 				<div className="row">
 					<div className="col-md-6" style={ { position: 'relative', left: '300px'} }>
 						<div className="bs-docs-section">
-						<Card title="Login">
-							Meu cartão
-						</Card>
+
+							<Card title="Login">
+								<div className="row">
+									<div className="col-lg-12">
+										<div className="bs-component">
+
+											<fieldset>
+												<FormGroup label="Email  *" htmlFor="email">
+													<input id="email" type="email" className="form-control" 
+													aria-describedby="emailHelp"
+													placeholder="Digite seu e-mail" value={this.state.email}
+													onChange={e => this.setState({email: e.target.value})}/>
+												</FormGroup>
+
+												<FormGroup label="Senha  *" htmlFor="senha">
+													<input id="senha" type="password" className="form-control" aria-describedby="senhaHelp" 
+													placeholder="Digite sua senha" value={this.state.senha} 
+													onChange={e => this.setState({senha: e.target.value})}/>
+												</FormGroup>
+											</fieldset>
+
+											<button onClick={this.entrar} className="btn btn-success" 
+											style={{ marginRight: '5px' }}>Entrar</button>
+											<button className="btn btn-danger">Cancelar</button>
+
+										</div>
+									</div>
+								</div>
+							</Card>
+
 						</div>
 					</div>
 				</div>
