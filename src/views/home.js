a03b04875a7e32bc572c23996ba8dfcc936d7232
Nathalia <nathreboucas5@gmail.com>
2020-02-10 21:03:52 -0300

    link cadastro lançamento da tela home

--- src/views/home.js
+++ src/views/home.js
@@ -33,7 +33,11 @@ class Home extends React.Component {
                 <p>Essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
                 <p className="lead">
                     <a className="btn btn-primary btn-lg margin-right" href="#/cadastro-usuario" role="button"><i className="fa fa-users"></i>  Cadastrar Usuário</a>
-                    <a className="btn btn-danger btn-lg" href="https://bootswatch.com/flatly/#" role="button"><i className="fa fa-users"></i>  Cadastrar Lançamento</a>
+                    <a className="btn btn-danger btn-lg" role="button"
+                    href="#/cadastro-lancamento">
+                        <i className="fa fa-users"></i>
+                        Cadastrar Lançamento
+                    </a>
                 </p>
             </div>
         )
