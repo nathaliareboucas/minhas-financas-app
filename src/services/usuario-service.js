d17cedbf92b5460155ef77677aa640cfb44741f7
Nathalia <nathreboucas5@gmail.com>
2020-02-11 22:38:10 -0300

    refatorando validação do cadastro de usuário

--- src/services/usuario-service.js
+++ src/services/usuario-service.js
@@ -1,6 +1,8 @@
 import ApiService from '../app/api-service'
 import LocalStorageService from './localStorage-service'
 
+import ErroValidacao from '../app/exception/ErroValidacao'
+
 class UsuarioService extends ApiService {
 
     constructor() {
@@ -25,6 +27,30 @@ class UsuarioService extends ApiService {
 
     salvar(usuario) {
         return this.post('', usuario)
+    }
+
+    validar(usuario) {
+        const erros = []
+
+		if (!usuario.nome) erros.push('O campo nome é obrigatório.')
+		
+		if (!usuario.email) {
+			erros.push('O campo email é obrigatório.')
+		} else if (!usuario.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)) {
+			erros.push('Informe um email válido.')
+		}
+
+		if (!usuario.senha) erros.push('O campo senha é obrigatório.')
+		
+		if (!usuario.senhaRepeticao) {
+			erros.push('O campo repita a senha é obrigatório.')
+		} else if (usuario.senhaRepeticao !== usuario.senha) {
+			erros.push('As senhas devem ser iguais.')
+		}
+
+		if (erros && erros.length > 0) {
+            throw new ErroValidacao(erros)
+        }
     }
 }
 
